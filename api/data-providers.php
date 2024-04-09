6ddaf689db18c7e0e5e799d308663843de628458
Thomas Tursics <thomas@tursics.de>
2024-04-09 21:24:08 +0200

    add POST endpoint for /data-providers

--- api/data-providers.php
+++ api/data-providers.php
@@ -4,6 +4,25 @@
     header('Access-Control-Allow-Headers: X-Requested-With');
 	header('Content-Type: application/json; charset=utf-8');
 
+	if ('POST' === $_SERVER['REQUEST_METHOD']) {
+	header('HTTP/1.0 400 Bad Request');
+		echo json_encode((object) array(
+			'error' => 400,
+			'message' => 'Bad Request. Please create an issue on GitHub for your change request',
+			'createIssue' => 'https://github.com/opendata-guru/data-universal-repository-api/issues/new',
+			'repository' => 'https://github.com/opendata-guru/data-universal-repository-api/tree/main/api-data',
+		));
+		return;
+	}
+	if ('GET' !== $_SERVER['REQUEST_METHOD']) {
+		header('HTTP/1.0 405 Method Not Allowed');
+		echo json_encode((object) array(
+			'error' => 405,
+			'message' => 'Method Not Allowed. HTTP verb used to access this page is not allowed',
+		));
+		return;
+	}
+
 	include('suppliers/_semantic.php');
 
 	$dataProviders = [];
