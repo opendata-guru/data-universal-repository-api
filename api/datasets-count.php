fb9fe4b7606e18f62f0444ac84d5fb91eef554f5
Thomas Tursics <thomas@tursics.de>
2024-05-22 16:54:38 +0200

    add pid parameter support to /datasets/count endpoint

--- api/datasets-count.php
+++ api/datasets-count.php
@@ -5,9 +5,37 @@
 	header('Content-Type: application/json; charset=utf-8');
 
 	include('helper/_link.php');
+	include('helper/_provider.php');
 
 	$link = getLink();
+	$provider = getProvider();
+	$pid = '';
 
+	if (($link->parameter !== '') && ($provider->parameter !== '')) {
+		header('HTTP/1.0 400 Bad Request');
+		echo json_encode((object) array(
+			'error' => 400,
+			'message' => 'Bad Request. Both parameters \'link\' and \'pID\' are set',
+		));
+		exit;
+	}
+	if ($provider->parameter !== '') {
+		if ($provider->error) {
+			header($provider->error->header);
+			echo json_encode((object) array(
+				'error' => $provider->error->error,
+				'message' => $provider->error->message,
+			));
+			exit;
+		}
+		$link = getLinkWithParam($provider->url);
+		$pid = $provider->parameter;
+	} else {
+		$pObject = findPObjectByLink($link);
+		if ($pObject) {
+			$pid = providerGetPID($pObject);
+		}
+	}
 	if ($link->error) {
 		header($link->error->header);
 		echo json_encode((object) array(
