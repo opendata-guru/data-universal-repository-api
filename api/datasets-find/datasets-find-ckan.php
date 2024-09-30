9750011891b409bed63d20a81b8878732031c099
Thomas Tursics <thomas@tursics.de>
2024-09-30 21:32:06 +0200

    document next steps

--- api/datasets-find/datasets-find-ckan.php
+++ api/datasets-find/datasets-find-ckan.php
@@ -2,6 +2,10 @@
 	function datasetsFindCKAN($url, $identifier) {
 		$packageSearchSuffix = '/api/3/action/package_search?q=identifier:';
 
+		// Q: Can you find records using keywords using our CKAN API?
+		// A: Yes, that is possible. Here is an example:
+		//    _path_to_/ckan/api/3/action/package_search?fq=tags:economy
+
 		$json = json_decode(get_contents($url . $packageSearchSuffix . $identifier));
 		$results = [];
 
