bc0b0273ca51d2e36bc02218126a727d1f7fc5ab
Thomas Tursics <thomas@tursics.de>
2024-10-24 20:26:53 +0200

    interpret title of assets

--- api/live-insights/live-insights-interpreter.php
+++ api/live-insights/live-insights-interpreter.php
@@ -3,11 +3,27 @@
 	// https://www.inspire.niedersachsen.de/doorman/noauth/alkis-dls-elu?REQUEST=GetCapabilities&SERVICE=WFS
 
 	$semRegistry = array(
+//		'uri' => 'https://inspire.ec.europa.eu/featureconcept/ExistingLandUseDataSet',
 		(object) array(
-			'id' => 'elu:ExistingLandUseObject',
+			'ids' => ['elu:ExistingLandUseObject'],
 			'uri' => 'https://inspire.ec.europa.eu/schemas/elu/4.0/ExistingLandUse.xsd',
 			'title' => 'existing land use object',
+			'title_de' => 'Existierende Bodennutzung',
 			'definition' => 'An existing land use object describes the land use of an area having a homogeneous combination of land use types.',
+		),
+		(object) array(
+			'ids' => ['GE.GeologicFault'],
+			'uri' => 'https://inspire.ec.europa.eu/layer/GE.GeologicFault',
+			'title' => 'Geologic faults',
+			'title_de' => 'Geologische Verwerfungen',
+			'definition' => 'This layer only applies to MappedFeature spatial objects whose specification property is of type ShearDisplacementStructure.',
+		),
+		(object) array(
+			'ids' => ['hy:DrainageBasin', 'hy-p:DrainageBasin'],
+			'uri' => 'https://inspire.ec.europa.eu/featureconcept/DrainageBasin',
+			'title' => 'Drainage Basin',
+			'title_de' => 'Wassereinzugsgebiet',
+			'definition' => 'Area having a common outlet for its surface runoff.',
 		),
 	);
 
@@ -18,14 +34,24 @@
 		$assets = [];
 
 		if ($content && $content->body) {
-			if ($content && $content->body->assets) {
-				foreach($content->body->assets as $asset) {
+			$bodyAssets = $content->body->assets ? $content->body->assets : $content->body['assets'];
+			if ($bodyAssets) {
+				foreach($bodyAssets as $asset) {
 					foreach($semRegistry as $entry) {
-						if ($entry->id === $asset->name) {
+						if ($asset->name && in_array($asset->name, $entry->ids)) {
 							$assets[] = (object) array(
 								'id' => $entry->uri,
-								'title' => $entry->title,
+								'title' => (object) ['de' => $entry->title_de, 'en' => $entry->title],
 							);
+						}
+
+						foreach($entry->ids as $id) {
+							if (str_contains($asset->title, $id)) {
+								$assets[] = (object) array(
+									'id' => $entry->uri,
+									'title' => (object) ['de' => $entry->title_de, 'en' => $entry->title],
+								);
+							}
 						}
 					}
 				}
