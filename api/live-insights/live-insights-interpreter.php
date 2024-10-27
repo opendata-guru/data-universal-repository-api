288749bf58b9000522de6ea8f3e0ea821dac2f96
Thomas Tursics <thomas@tursics.de>
2024-10-27 21:09:39 +0100

    add Address schemas

--- api/live-insights/live-insights-interpreter.php
+++ api/live-insights/live-insights-interpreter.php
@@ -4,6 +4,35 @@
 
 	$semRegistry = array(
 		(object) array(
+			'ids' => ['AD.Address', 'ad:Address'],
+			'uri' => 'https://inspire.ec.europa.eu/layer/AD.Address',
+			'title' => 'Addresses',
+			'title_de' => 'Adressen',
+			'definition' => '',
+			'spatialobjecttype' => 'Address',
+		),
+		(object) array(
+			'ids' => ['ad:AdminUnitName'],
+			'uri' => 'https://inspire.ec.europa.eu/schemas/ad/4.0/Addresses.xsd#AdminUnitName',
+			'title' => 'Name of the administrative unit',
+			'title_de' => 'Name der Verwaltungseinheit',
+			'definition' => 'An address component which represents the name of a unit of administration where a Member State has and/or exercises jurisdictional rights, for local, regional and national governance.',
+		),
+		(object) array(
+			'ids' => ['ad:PostalDescriptor'],
+			'uri' => 'https://inspire.ec.europa.eu/schemas/ad/4.0/Addresses.xsd#PostalDescriptor',
+			'title' => 'Postal Description',
+			'title_de' => 'Postalische Beschreibung',
+			'definition' => 'An address component which represents the identification of a subdivision of addresses and postal delivery points in a country, region or city for postal purposes.',
+		),
+		(object) array(
+			'ids' => ['ad:ThoroughfareName'],
+			'uri' => 'https://inspire.ec.europa.eu/schemas/ad/4.0/Addresses.xsd#ThoroughfareName',
+			'title' => 'Name of the thoroughfare',
+			'title_de' => 'Name des Verkehrswegs',
+			'definition' => 'An address component which represents the name of a passage or way through from one location to another.',
+		),
+		(object) array(
 			'ids' => ['au:AdministrativeBoundary'],
 			'uri' => 'https://inspire.ec.europa.eu/featureconcept/AdministrativeBoundary',
 			'title' => 'Administrative Boundary',
@@ -17,17 +46,10 @@
 			'title_de' => 'Verwaltungseinheit',
 			'definition' => 'Unit of administration where a Member State has and/or exercises jurisdictional rights, for local, regional and national governance.',
 		),
-		(object) array(
-			'ids' => ['tn-ro:RoadLink'],
-			'uri' => 'https://inspire.ec.europa.eu/schemas/tn-ro/4.0/RoadTransportNetwork.xsd',
-			'title' => 'Road link',
-			'title_de' => 'Straßenverbindung',
-			'definition' => 'A linear spatial object that describes the geometry and connectivity of a road network between two points in the network. Road links can represent paths, bicycle roads, single carriageways, multiple carriageway roads and even fictitious trajectories across traffic squares.',
-		),
 //		'uri' => 'https://inspire.ec.europa.eu/featureconcept/ExistingLandUseDataSet',
 		(object) array(
 			'ids' => ['elu:ExistingLandUseObject'],
-			'uri' => 'https://inspire.ec.europa.eu/schemas/elu/4.0/ExistingLandUse.xsd',
+			'uri' => 'https://inspire.ec.europa.eu/schemas/elu/4.0/ExistingLandUse.xsd#ExistingLandUseObject',
 			'title' => 'existing land use object',
 			'title_de' => 'Existierende Bodennutzung',
 			'definition' => 'An existing land use object describes the land use of an area having a homogeneous combination of land use types.',
@@ -38,6 +60,14 @@
 			'title' => 'Geologic faults',
 			'title_de' => 'Geologische Verwerfungen',
 			'definition' => 'This layer only applies to MappedFeature spatial objects whose specification property is of type ShearDisplacementStructure.',
+		),
+		(object) array(
+			'ids' => ['GN.GeographicalNames'],
+			'uri' => 'https://inspire.ec.europa.eu/layer/GN.GeographicalNames',
+			'title' => 'Geographical names',
+			'title_de' => 'Geografische Bezeichnungen',
+			'definition' => '',
+			'spatialobjecttype' => 'NamedPlace',
 		),
 		(object) array(
 			'ids' => ['hy:DrainageBasin', 'hy-p:DrainageBasin'],
@@ -76,12 +106,11 @@
 			'definition' => 'A man-made raised long mound of earth or other material.',
 		),
 		(object) array(
-			'ids' => ['GN.GeographicalNames'],
-			'uri' => 'https://inspire.ec.europa.eu/layer/GN.GeographicalNames',
-			'title' => 'Geographical names',
-			'title_de' => 'Geografische Bezeichnungen',
-			'definition' => '',
-			'spatialobjecttype' => 'NamedPlace',
+			'ids' => ['tn-ro:RoadLink'],
+			'uri' => 'https://inspire.ec.europa.eu/schemas/tn-ro/4.0/RoadTransportNetwork.xsd#RoadLink',
+			'title' => 'Road link',
+			'title_de' => 'Straßenverbindung',
+			'definition' => 'A linear spatial object that describes the geometry and connectivity of a road network between two points in the network. Road links can represent paths, bicycle roads, single carriageways, multiple carriageway roads and even fictitious trajectories across traffic squares.',
 		),
 	);
 
