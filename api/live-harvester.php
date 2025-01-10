774062bbd79028d81876dc70e23071f4971417fd
Thomas Tursics <thomas@tursics.de>
2025-01-10 08:14:46 +0100

    prepare 2 new endpoints (/p/issues/{date} and /live/harvester)

---
+++ api/live-harvester.php
@@ -0,0 +1,16 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET');
+    header('Access-Control-Allow-Headers: X-Requested-With');
+	header('Content-Type: application/json; charset=utf-8');
+
+	$ret = 'empty';
+
+	// ckan
+	// https://ckan.govdata.de/harvest/about/open-data-brandenburg-harvester
+
+	// entryscape
+	// https://register.opendata.sachsen.de/organization/475/information
+
+	echo json_encode($ret);
+?>
