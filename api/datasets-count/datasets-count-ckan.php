d87207e4d4e0be901c1bcbffc61515954717647a
Thomas Tursics <thomas@tursics.de>
2025-03-30 23:48:14 +0200

    fix new CKAN portal from RLP

--- api/datasets-count/datasets-count-ckan.php
+++ api/datasets-count/datasets-count-ckan.php
@@ -18,7 +18,8 @@
 
 	function datasetsCountCKAN($url) {
 		$packageShowSuffix_3 = '/api/3/action/package_search?rows=1&start=0';
-		$packageShowSuffix = '/api/action/package_search?rows=1&start=0';
+//		$packageShowSuffix = '/api/action/package_search?rows=1&start=0';
+		$packageShowSuffix = '/api/action/package_search?fq=(isopen%3A%22true%22)&rows=1';
 		$packageShowAllSuffix = '/api/3/action/package_search';
 		$resourcesShowSuffix = '/api/3/action/current_package_list_with_resources?limit=1000';
 
