d4d49356b6290a8b6c5fcd53e120695171befcfe
Thomas Tursics <thomas@tursics.de>
2025-04-21 11:40:56 +0200

    add GET /live/url endpoint

---
+++ api/live-url.php
@@ -0,0 +1,41 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET');
+    header('Access-Control-Allow-Headers: X-Requested-With');
+	header('Content-Type: application/json; charset=utf-8');
+
+//	include('live-insights/live-insights-get.php');
+
+	if ('GET' !== $_SERVER['REQUEST_METHOD']) {
+		header('HTTP/1.0 405 Method Not Allowed');
+		echo json_encode((object) array(
+			'error' => 405,
+			'message' => 'Method Not Allowed. HTTP verb used to access this page is not allowed',
+		));
+		return;
+	}
+
+	$parameterURL = html_entity_decode(trim(htmlspecialchars($_GET['url'])));
+
+	if ($parameterURL == '') {
+		header('HTTP/1.0 400 Bad Request');
+		echo json_encode((object) array(
+			'error' => 400,
+			'message' => 'Bad Request. Parameter \'url\' is not set',
+		));
+		exit;
+	}
+
+	$host = parse_url($parameterURL, PHP_URL_HOST);
+
+	if ('www.' == substr($host, 0, 4)) {
+		$host = substr($host, 4);
+	}
+
+	$ret = (object) array(
+		'host' => $host,
+		'url' => $parameterURL,
+	);
+
+	echo json_encode($ret);
+?>
