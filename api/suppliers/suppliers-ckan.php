4f61d70f5c2c51e61ee65e7dcbc231d5655d14f1
Thomas Tursics <thomas@tursics.de>
2026-02-02 23:53:08 +0100

    bugfix: return empty array for DKAN with no organisations

--- api/suppliers/suppliers-ckan.php
+++ api/suppliers/suppliers-ckan.php
@@ -75,13 +75,62 @@
 		}
 	}
 
+	function collectDataByDKAN($uriDomain, $url, $pid, &$data) {
+		$groupListSuffix = '/api/3/action/group_list';
+		$groupShowSuffix = '/api/3/action/group_show?id=';
+		$groupPackageShowSuffix = '/api/3/action/group_package_show?id=';
+		$groupWebsiteSuffix = '/group/';
+
+		$uri = $url . $groupListSuffix;
+//		$json = json_decode(file_get_contents($uri));
+		$json = json_decode(get_contents($uri));
+
+		if ($json) {
+			$jsonData = $json;
+			if (property_exists($jsonData, 'result')) {
+				$jsonData = $jsonData->result;
+			}
+
+			foreach($jsonData as $groupID) {
+				$uri = $url . $groupShowSuffix;
+//				$json = json_decode(file_get_contents($uri . $groupID->name));
+				$json = json_decode(get_contents($uri . $groupID->name));
+
+				if ($json) {
+					$uris = json_decode($json->result->extras[0]->value);
+					$data[] = semanticContributor($uriDomain, $pid, array(
+						'id' => $json->result->id,
+						'name' => $json->result->name,
+						'title' => $json->result->title,
+						'created' => $json->result->created,
+						'packages' => $json->result->package_count,
+						'uri' => $uris[0]
+					));
+				} else {
+/*					$uri = $url . $groupPackageShowSuffix;
+					$json = json_decode(get_contents($uri . $groupID->name));
+
+					if ($json) {
+						$uris = json_decode($json->result->extras[0]->value);
+						$data[] = semanticContributor($uriDomain, $pid, array(
+							'id' => $json->result->id,
+							'name' => $json->result->name,
+							'title' => $json->result->title,
+							'created' => $json->result->created,
+							'packages' => $json->result->package_count,
+							'uri' => $uris[0]
+						));
+					} else*/ {
+						$data[] = semanticContributor($uriDomain, $pid, scrapeWebsite($groupID->name, $url, $groupWebsiteSuffix));
+					}
+				}
+			}
+		}
+	}
+
 	function suppliersCKAN($url, $pid) {
 		$orgaListSuffix = '/api/3/action/organization_list';
 		$orgaShowSuffix = '/api/3/action/organization_show?id=';
-		$groupListSuffix = '/api/3/action/group_list';
-		$groupShowSuffix = '/api/3/action/group_show?id=';
-//		$groupPackageShowSuffix = '/api/3/action/group_package_show?id=';
-		$groupWebsiteSuffix = '/group/';
 
 		$uri = $url . $orgaListSuffix;
 //		$uriDomain = end(explode('/', $url));
@@ -146,56 +195,15 @@
 				));
 			}
 		} else {
-			// DKAN
-			$uri = $url . $groupListSuffix;
-//			$json = json_decode(file_get_contents($uri));
-			$json = json_decode(get_contents($uri));
-
-			if ($json) {
-				$jsonData = $json;
-				if ($json->result) {
-					$jsonData = $json->result;
-				}
-
-				foreach($jsonData as $groupID) {
-					$uri = $url . $groupShowSuffix;
-//					$json = json_decode(file_get_contents($uri . $groupID->name));
-					$json = json_decode(get_contents($uri . $groupID->name));
-
-					if ($json) {
-						$uris = json_decode($json->result->extras[0]->value);
-						$data[] = semanticContributor($uriDomain, $pid, array(
-							'id' => $json->result->id,
-							'name' => $json->result->name,
-							'title' => $json->result->title,
-							'created' => $json->result->created,
-							'packages' => $json->result->package_count,
-							'uri' => $uris[0]
-						));
-					} else {
-/*						$uri = $url . $groupPackageShowSuffix;
-						$json = json_decode(file_get_contents($uri . $groupID->name));
-
-						if ($json) {
-							$uris = json_decode($json->result->extras[0]->value);
-							$data[] = semanticContributor($uriDomain, $pid, array(
-								'id' => $json->result->id,
-								'name' => $json->result->name,
-								'title' => $json->result->title,
-								'created' => $json->result->created,
-								'packages' => $json->result->package_count,
-								'uri' => $uris[0]
-							));
-						} else*/ {
-							$data[] = semanticContributor($uriDomain, $pid, scrapeWebsite($groupID->name, $url, $groupWebsiteSuffix));
-						}
-					}
-				}
-			} else {
-				// page '403 Forbidden'
-
-				// todo
+			collectDataByDKAN($uriDomain, $url, $pid, $data);
+			if (count($data) > 0) {
+				echo json_encode($data);
+				return;
 			}
+
+			// page '403 Forbidden'
+
+			// todo
 		}
 
 		echo json_encode($data);
