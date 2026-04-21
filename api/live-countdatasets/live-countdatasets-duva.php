70f5e0ad5f9b06cc761c3886054f4c27690b00cd
Thomas Tursics <thomas@tursics.de>
2026-04-21 20:31:45 +0200

    add fallback and scrape dataset count from DUVA systems

--- api/live-countdatasets/live-countdatasets-duva.php
+++ api/live-countdatasets/live-countdatasets-duva.php
@@ -9,6 +9,23 @@
 		return $var->{'@type'} === 'dcat:Dataset';
 	}*/
 
+	function scrapeWebsite($html) {
+		$start = stripos($html, '<table>') + 7;
+		$end = stripos($html, '</table>', $start);
+		$length = $end - $start;
+		$html = trim(substr($html, $start, $length));
+
+		$body = explode('<tr>', $html);
+		if ($body[0] === '') {
+			array_shift($body);
+		}
+
+		// remove table header
+		array_shift($body);
+
+		return count($body);
+	}
+
 	function filterCatalog($var) {
 		return $var->{'@type'} === 'dcat:Catalog';
 	}
@@ -20,17 +37,23 @@
 //		$countSuffix = '/duva2dcat/catalog.rdf';
 //		$countSuffix = '/duva2dcat/dcat/catalog';
 		$dcatSuffix = '/dcat-ap/catalog.jsonld';
+		$dcatFileListSuffix = '/dcat-ap/';
 
 		$uri = $url . $countSuffix;
 		$json = json_decode(get_contents($uri));
+
+		$catalog = [];
+		$count = 0;
 
 		if ($json === null) {
 			$uri = $url . $dcatSuffix;
 			$json = json_decode(get_contents($uri));
 		}
 
-		$catalog = [];
-		$count = 0;
+		if ($json === null) {
+			$uri = $url . $dcatFileListSuffix;
+			$count = scrapeWebsite(get_contents($uri));
+		}
 
 		if ($json) {
 			$catalog = array_filter($json->{'@graph'}, 'filterCatalog');
